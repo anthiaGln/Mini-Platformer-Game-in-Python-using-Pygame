03df5c80c8c2d1ed4ed7e9f1e90e423ee38ca196
anthiaGln <56629690+anthiaGln@users.noreply.github.com>
2025-09-17 15:19:38 +0300

    Update tutorial.py

--- tutorial.py
+++ tutorial.py
@@ -12,6 +12,7 @@ pygame.display.set_caption("Platformer")
 
 BG_COLOR = (255, 255, 255)
 WIDTH, HEIGHT = 700, 500
+PLAYER_VEL = 5
 FPS = 60
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -23,6 +24,31 @@ class Player(pygame.sprite.Sprite):
         self.rect = pygame.Rect(x,y,width, height)
         self.x_vel = 0
         self.y_vel = 0
+        self.mask = None
+        self.direction = "left"
+        self.animation_count = 0
+    
+    def move(self, dx, dy):
+        self.rect.x +=dx
+        self.rect.y+= dy
+
+    def move_left(self,vel):
+        self.x_vel = -vel
+        if self.direction != "left":
+            self.direction = "left"
+            self.animation_count = 0
+
+    def move_right(self,vel):
+            self.x_vel = vel
+            if self.direction != "right":
+                self.direction = "right"
+                self.animation_count = 0
+
+    def loop(self,fps):
+        self.move(self.x_vel, self.y_vel)
+    
+    def draw(self, win):
+        pygame.draw.rect(win, self.COLOR, self.rect)
 
 def get_background(name):
     image = pygame.image.load(join("assets", "Background", name))
@@ -36,30 +62,50 @@ def get_background(name):
 
     return tiles, image
 
-def draw(window, background, bg_image):
+def draw(window, background, bg_image, player):
     for tile in background:
         window.blit(bg_image, tuple(tile))
     
+    player.draw(window)
     pygame.display.update()
+
+#---------------------------------
+#separete movement in a function
+#---------------------------------
+def handle_move(player):
+    keys = pygame.key.get_pressed()
+
+    # i want my player to move when the key is held down
+    player.x_vel = 0
+    if keys[pygame.K_LEFT]:
+        player.move_left(PLAYER_VEL)
+    
+    if keys[pygame.K_RIGHT]:
+        player.move_right(PLAYER_VEL)
+    
 
 def main(window):
     clock = pygame.time.Clock()
     background, bg_image = get_background("Purple.png")
+
+    player = Player(100, 100, 50,50)
+
     run = True
 
     while run:
         clock.tick(FPS)
 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:  
+            if event.type == pygame.QUIT:  # when you click "X"
                 run = False
                 break
-        
-        draw(window, background,bg_image)
+
+        player.loop(FPS)
+        handle_move(player)
+        draw(window, background,bg_image, player)
 
 
     pygame.quit()
     sys.exit()
 if __name__ == "__main__":
-
     main(window)
